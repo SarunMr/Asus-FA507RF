3b9d56ca876711a25c5dc1a50d4894610efc209f
SarunMr <sarunmaharjan38@gmail.com>
2025-03-14 13:03:19 +0545

    lsp_started

---
+++ apps/nixvim/lsp_plugins/nvim-jdtls.nix
@@ -0,0 +1,7 @@
+{...}:
+
+{
+  programs.nixvim = {
+    plugins.nvim-jdtls= true;
+  };
+}
---
+++ apps/nixvim/lsp_plugins/nvim-lspconfig.nix
@@ -0,0 +1,28 @@
+{...}:
+
+{
+    programs.nixvim ={
+      plugins.lsp ={
+        enable = true;
+        servers ={
+          lua_ls.enable=true;
+          pyright.enable=true;
+          nixd.enable=true;
+          bashls.enable=true;
+          
+          #Web Devlopment
+          html.enable = true;
+          cssls.enable = true;
+          ts_ls.enable = true;
+          eslint.enable = true;
+      };
+    };
+    keymaps.lspBuf = {
+        "gd" = "definition";
+        "gD" = "references";
+        "gt" = "type_definition";
+        "gi" = "implementation";
+        "K" = "hover";
+      };
+  };
+}
