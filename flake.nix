9330b1bfd8deaf2544c878fb0d397ae704dd03ca
SarunMr <sarunmaharjan38@gmail.com>
2025-03-31 10:54:22 +0545

    adding unstable branch done

--- flake.nix
+++ flake.nix
@@ -4,39 +4,43 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; # Add unstable packages
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = { url = "github:nix-community/nixvim"; };
   };
 
-  outputs = { nixpkgs,nixpkgs-unstable, home-manager, nixvim, ... }@inputs:
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, nixvim, ... }@inputs:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgsUnstable = nixpkgs-unstable.legacyPackages.${system};
+      pkgsUnstable = nixpkgs-unstable.legacyPackages.${system}; # Extract unstable packages
     in {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
           inherit system;
-          modules = [ ./configuration.nix
+          modules = [
+            ./configuration.nix
             {
-              nixpkgs.config.allowUnfree = true;
-              _module.args.unstablePkgs = pkgsUnstable; # Pass to configuration.nix
+              nixpkgs.config.allowUnfree = true; # Allow unfree packages
+              _module.args.unstablePkgs = pkgsUnstable; # Pass unstablePkgs to NixOS config
             }
           ];
-
         };
       };
+
       homeConfigurations."sarun" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix 
-          nixvim.homeManagerModules.nixvim 
-         {
-            _module.args.unstablePkgs = pkgsUnstable; # Pass to home.nix
+        modules = [
+          ./home.nix
+          nixvim.homeManagerModules.nixvim
+          {
+            _module.args.unstablePkgs = pkgsUnstable; # Pass unstablePkgs to Home Manager
           }
         ];
       };
